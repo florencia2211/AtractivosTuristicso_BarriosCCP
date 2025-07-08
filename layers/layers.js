8bffebf040d9de959c4a618150e7aba6d3d3e5da
florencia2211 <flrivas2022@udec.cl>
2025-07-07 22:16:45 -0400

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -64,13 +64,13 @@ var lyr_AtractivosTuristicos_2 = new ol.layer.Vector({
     <img src="styles/legend/AtractivosTuristicos_2_19.png" /> Templo Pedro de Valdivia<br />\
     <img src="styles/legend/AtractivosTuristicos_2_20.png" /> <br />' });
 
-lyr_HereWegoTerrain_0.setVisible(true);lyr_BARRIOS_SELECCIONADOS_1.setVisible(true);lyr_AtractivosTuristicos_2.setVisible(true);
+lyr_HereWegoTerrain_0.setVisible(true);lyr_BARRIOS_SELECCIONADOS_1.setVisible(false);lyr_AtractivosTuristicos_2.setVisible(true);
 var layersList = [lyr_HereWegoTerrain_0,lyr_BARRIOS_SELECCIONADOS_1,lyr_AtractivosTuristicos_2];
 lyr_BARRIOS_SELECCIONADOS_1.set('fieldAliases', {'Id': 'Id', 'Nombre': 'Nombre', 'Area': 'Area', 'hab_m2': 'hab_m2', 'Sum_Poblac': 'Sum_Poblac', 'av_hab': 'av_hab', 'Long_tLic': 'Long_tLic', 'Long_tNLic': 'Long_tNLic', 'Suma_Trans': 'Suma_Trans', 'porc_cober': 'porc_cober', 'Distance': 'Distance', 'LongcicloK': 'LongcicloK', 'Long_rvial': 'Long_rvial', });
 lyr_AtractivosTuristicos_2.set('fieldAliases', {'NOMBRE': 'NOMBRE', 'TIPO': 'TIPO', 'BARRIO': 'BARRIO', 'VIDEO': 'VIDEO', 'InfoSitio': 'InfoSitio', 'Horario': 'Horario', });
 lyr_BARRIOS_SELECCIONADOS_1.set('fieldImages', {'Id': 'Range', 'Nombre': 'TextEdit', 'Area': 'TextEdit', 'hab_m2': 'TextEdit', 'Sum_Poblac': 'Range', 'av_hab': 'TextEdit', 'Long_tLic': 'TextEdit', 'Long_tNLic': 'TextEdit', 'Suma_Trans': 'TextEdit', 'porc_cober': 'TextEdit', 'Distance': 'TextEdit', 'LongcicloK': 'TextEdit', 'Long_rvial': 'TextEdit', });
 lyr_AtractivosTuristicos_2.set('fieldImages', {'NOMBRE': 'TextEdit', 'TIPO': 'TextEdit', 'BARRIO': 'TextEdit', 'VIDEO': 'TextEdit', 'InfoSitio': 'TextEdit', 'Horario': 'TextEdit', });
-lyr_BARRIOS_SELECCIONADOS_1.set('fieldLabels', {'Id': 'no label', 'Nombre': 'no label', 'Area': 'no label', 'hab_m2': 'no label', 'Sum_Poblac': 'no label', 'av_hab': 'no label', 'Long_tLic': 'no label', 'Long_tNLic': 'no label', 'Suma_Trans': 'no label', 'porc_cober': 'no label', 'Distance': 'no label', 'LongcicloK': 'no label', 'Long_rvial': 'no label', });
+lyr_BARRIOS_SELECCIONADOS_1.set('fieldLabels', {'Id': 'no label', 'Nombre': 'no label', 'Area': 'inline label - always visible', 'hab_m2': 'no label', 'Sum_Poblac': 'no label', 'av_hab': 'no label', 'Long_tLic': 'no label', 'Long_tNLic': 'no label', 'Suma_Trans': 'no label', 'porc_cober': 'no label', 'Distance': 'no label', 'LongcicloK': 'no label', 'Long_rvial': 'no label', });
 lyr_AtractivosTuristicos_2.set('fieldLabels', {'NOMBRE': 'inline label - always visible', 'TIPO': 'inline label - always visible', 'BARRIO': 'inline label - always visible', 'VIDEO': 'inline label - always visible', 'InfoSitio': 'inline label - always visible', 'Horario': 'inline label - always visible', });
 lyr_AtractivosTuristicos_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
